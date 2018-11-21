27e222199cb08576187a8c9edb835d59b34cf4c8
WangJun <1127wangjun@gmail.com>
2018-11-21 12:31:49 -0600

    testcaseGenerator

---
+++ src/main/java/edu/ut/verify/core/testcaseGenerator.java
@@ -0,0 +1,11 @@
+package edu.ut.verify.core;
+
+public class testcaseGenerator {
+    private StateMachine stateMachine;
+
+    public testcaseGenerator(StateMachine sm){
+        this.stateMachine = sm;
+    }
+
+
+}
