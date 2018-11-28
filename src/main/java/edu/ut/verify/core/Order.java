0cf5b85144d1bee71b9d5c3e397bb9ee6da041e7
WangJun <1127wangjun@gmail.com>
2018-11-28 10:54:33 -0600

    path order add

---
+++ src/main/java/edu/ut/verify/core/Order.java
@@ -0,0 +1,23 @@
+package edu.ut.verify.core;
+
+import java.util.ArrayList;
+
+public class Order {
+    ArrayList<String[]> order;
+
+    public Order(){
+        this.order = new ArrayList<>();
+    }
+
+    public void put(String[] ord){
+        this.order.add(ord);
+    }
+
+    public ArrayList<String[]> getOrder() {
+        return order;
+    }
+
+    public void setOrder(ArrayList<String[]> order) {
+        this.order = order;
+    }
+}
